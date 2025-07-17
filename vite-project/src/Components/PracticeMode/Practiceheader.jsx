007087cb1a3531023be48c670c85810c3d41b907
Ongeri123 <ongerinewton10@gmail.com>
2025-07-17 22:11:42 +0300

    WB-11 <added practice components>

---
+++ vite-project/src/Components/PracticeMode/Practiceheader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PracticeHeader = () => {
+    return (
+        <div className="practice-header">
+            <h2>PRACTICE WORDLE</h2>
+        </div>
+    )
+}
+
+export default PracticeHeader
