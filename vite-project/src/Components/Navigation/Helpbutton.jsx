5e8aeaccb37d7afb281376370c76f6b2a333c997
Ongeri123 <ongerinewton10@gmail.com>
2025-07-18 23:28:41 +0300

    WB-11 added helpbutton

---
+++ vite-project/src/Components/Navigation/Helpbutton.jsx
@@ -0,0 +1,11 @@
+// import PracticeMode from './PracticeMode/PracticeMode';
+
+function Helpbutton() {
+    return (
+        <div>
+            <button>Help</button>
+        </div>
+    )
+}
+
+export default Helpbutton;
